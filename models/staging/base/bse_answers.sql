9de8ab137a1dfa5a50952374431f675aa5704196
jdarrell <jwdarrell@gmail.com>
2020-06-28 16:36:30 -0600

    new base model for answers

---
+++ models/staging/base/bse_answers.sql
@@ -0,0 +1,30 @@
+{{ config(tags = ["stackoverflow","base"]) }}
+
+with source as (
+
+    select * from {{ source('stackoverflow', 'answers') }}
+
+),
+base as (
+
+    select
+
+        id as answer_id,
+        last_editor_user_id,
+        owner_user_id,
+        parent_id as question_id,
+
+        body,
+        comment_count,
+        owner_display_name,
+        score,
+
+        creation_date as created_at,
+        last_activity_date as last_activity_at,
+        last_edit_date as last_edit_at
+
+    from source
+
+)
+
+select * from base
