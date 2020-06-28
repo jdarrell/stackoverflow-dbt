b12aa846f160d40d73f3b78b1e46533aeadb127c
jdarrell <jwdarrell@gmail.com>
2020-06-28 16:06:13 -0600

    new base model for questions

---
+++ models/staging/base/bse_questions.sql
@@ -0,0 +1,36 @@
+{{ config(tags = ["stackoverflow","base"]) }}
+
+with source as (
+
+    select * from {{ source('stackoverflow', 'questions') }}
+
+),
+base as (
+
+    select
+
+        id as question_id,
+        accepted_answer_id,
+        last_editor_user_id,
+        owner_user_id,
+
+        answer_count,
+        body,
+        comment_count,
+        coalesce(favorite_count, 0) as favorite_count,
+        owner_display_name,
+        tags,
+        title,
+        score,
+        view_count,
+
+        community_owned_date as community_owned_at,
+        creation_date as created_at,
+        last_activity_date as last_activity_at,
+        last_edit_date as last_edit_at
+
+    from source
+
+)
+
+select * from base
