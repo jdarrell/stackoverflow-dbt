dfcee72bc21d6364acd58399e7b1f5c5e35dd2bc
jdarrell <jwdarrell@gmail.com>
2020-06-28 21:47:14 -0600

    filters for data 2014 and newer

--- models/marts/core/fct_answers.sql
+++ models/marts/core/fct_answers.sql
@@ -22,10 +22,13 @@ joined as (
         answers.*,
 
         if(accepted_answers.accepted_answer_id is not null, true, false) is_accepted_answer,
-        row_number() over (partition by question_id order by created_at) as answer_sequence
+        row_number() over (partition by answers.question_id order by answers.created_at) as answer_sequence
 
     from answers
     left join accepted_answers on answers.answer_id = accepted_answers.accepted_answer_id
+    left join questions on answers.question_id = questions.question_id
+
+    where questions.created_at >= '2014-01-01'
 
 )
 
