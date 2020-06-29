9bebc783ffe1d6a4d77b938276b9729831ddd8cf
jdarrell <jwdarrell@gmail.com>
2020-06-28 18:50:25 -0600

    new fact model for answers

---
+++ models/marts/core/fct_answers.sql
@@ -0,0 +1,32 @@
+{{ config(tags = ["stackoverflow","fact"]) }}
+
+with answers as (
+
+    select * from {{ ref('bse_answers') }}
+
+),
+questions as (
+
+    select * from {{ ref('bse_questions') }}
+
+),
+accepted_answers as (
+
+    select accepted_answer_id from questions where accepted_answer_id is not null
+
+),
+joined as (
+
+    select
+
+        answers.*,
+
+        if(accepted_answers.accepted_answer_id is not null, true, false) is_accepted_answer,
+        row_number() over (partition by question_id order by created_at) as answer_sequence
+
+    from answers
+    left join accepted_answers on answers.answer_id = accepted_answers.accepted_answer_id
+
+)
+
+select * from joined
